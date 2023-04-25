0e45c063783b99f462ed503fce4d6ac344179278
enhancedlearningwithvr <130221090+enhancedlearningwithvr@users.noreply.github.com>
2023-04-26 05:17:55 +0530

    Created API endpoint at /

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000")
+})
+
+
+app.get("/", (_, res) => {
+    res.status(200).send("API Online!")
+})
