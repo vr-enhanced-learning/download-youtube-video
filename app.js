11d461dc10d21892b1b472643980ad08f976073d
Tharunraj Optimus <tharunoptimus@outlook.com>
2023-04-26 00:07:00 +0000

    Added endpoint to download and respond with video * Auto deletes video on response

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express")
+const youtubedl = require("youtube-dl-exec")
+const path = require("path")
+const fs = require("fs").promises
+
 const app = express()
 
 app.use(express.json())
@@ -13,3 +17,44 @@ app.get("/", (_, res) => {
     res.status(200).send("API Online!")
 })
 
+
+app.get("/download/:id", async (req, res) => {
+    const origin = req.headers.origin
+    const videoId = req.params.id
+    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`
+    const options = {
+        output: `${videoId}.mp4`,
+    }
+
+    try {
+        await youtubedl(videoUrl, options)
+        const absPath = path.resolve(`./${videoId}.mp4`)
+
+        if (origin != undefined) {
+            res.setHeader("Access-Control-Allow-Origin", origin)
+            res.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE",
+                "OPTIONS"
+            )
+            res.setHeader(
+                "Access-Control-Allow-Headers",
+                "Content-Type, Authorization"
+            )
+            res.setHeader("Access-Control-Allow-Credentials", true)
+            res.setHeader("Vary", "Origin")
+        }
+
+        res.sendFile(absPath, async () => {
+            try {
+                await fs.unlink(absPath)
+                console.log(`Deleted file: ${absPath}`)
+            } catch (err) {
+                console.error(err)
+            }
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Failed to download video")
+    }
+})
